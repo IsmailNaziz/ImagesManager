e4965910872a8024ce29d671f15f3120cd1fdd69
Ismail Naziz <ismailnaziz2@gmail.com>
2022-06-13 22:20:42 +0200

    Initial commit

---
+++ back_end.py
@@ -0,0 +1,15 @@
+from PyQt5.QtCore import QThread, pyqtSignal
+import time
+
+
+class AppBackEnd(QThread):
+	loading = pyqtSignal(int)
+
+	def __init__(self):
+		super(AppBackEnd, self).__init__()
+
+	def run(self):
+		for index in range(0, 101):
+			self.loading.emit(index)
+			time.sleep(0.1)
+
---
+++ front_end.py
@@ -0,0 +1,29 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+import back_end
+
+import os
+
+
+class AppFrontEnd(QMainWindow):
+	def __init__(self):
+		super(AppFrontEnd, self).__init__()
+		uic.loadUi(os.path.join(os.path.split(__file__)[0], "app_front_end.ui"), self)
+		self.app_backend_object = back_end.AppBackEnd()
+		self.connect_widgets()
+		self.show()
+
+	def connect_widgets(self):
+		self.start_button.clicked.connect(self.start_loading)
+		self.app_backend_object.loading.connect(self.update_progress_bar)
+
+	def update_progress_bar(self, value):
+		self.app_progress_bar.setValue(value)
+
+	def start_loading(self):
+		self.app_backend_object.start()
+
+def run_app_frontend():
+	front_app = QApplication([])
+	app_frontend_object = AppFrontEnd()
+	front_app.exec_()
